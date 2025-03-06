dc38484839a3a99193225160834331b3e807c65e
zhanghongfeng <youdianre123@gmail.com>
2025-03-06 13:24:22 +0100

    adjust the width of the figure

--- plot_dna_alignment.py
+++ plot_dna_alignment.py
@@ -29,7 +29,8 @@ def plot(res_phy, res, short_read, time_data, expected_index="1001"):
         kv = arr_phy[i].split(":")
         data_phy[int(kv[0], 2)] = float(kv[1])
 
-    x = np.array([i + width for i in range(length - 1)])
+    # x = np.array([i + width for i in range(length - 1)])
+    x = np.array([i for i in range(length - 1)])
     x_phy = np.array([i for i in range(length - 1)])
     time_qiskit = "running time: %s s " % (str(time_data[0]))
     time_qiskit_statevector = "running time: %s s " % (str(time_data[1]))
@@ -60,9 +61,6 @@ def get_index(res):
 
 
 def get_string(data):
-    # a1 = data.strip()[1:-1]
-    # a1 = a1.decode()
-    # a1 = a1.replace("'", "")
     return data
 
 
@@ -72,27 +70,6 @@ def code_sequence(code):
     for i in range(len(code)):
         res.append(nucledite[code[i]])
     return "".join(res)
-
-
-def ssh_python(reads):
-    import select
-    import subprocess
-    import time
-    # filename = output_path + output_name
-    aa = 'python3 sequence_alignment_ssh.py %s' % reads
-    f = subprocess.Popen(['ssh', 'hzhang@192.168.1.21', aa], \
-                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    p = select.poll()
-    p.register(f.stdout)
-    time.sleep(5)
-    test = []
-    count = 0
-    while count < 3 * 2:
-        count += 1
-        if p.poll(1):
-            test.append(f.stdout.readline())
-    # print(test)
-    return test
 
 
 def get_time_data(test):
@@ -106,14 +83,14 @@ warnings.filterwarnings("ignore", category=UserWarning)
 print("========================", "0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 ")
 print("========================", "|  |  |  |  |  |  |  |  |  |  |  |  |  |  |  ")
 print("The reference sequence: ", "A  T  T  G  T  C  T  A  G  G  C  G  A  C  C  A")
-p = input("Please input short read: ")
+p = input("Please input short read (type q or Q, then quit): ")
 while p not in ['q', "Q"]:
     encode = sequence_alignment.code_sequence(p)
     data = sequence_alignment.sequence_alignment(encode)
     time_data = data[1]
     plot(get_string(data[0][0]), get_string(data[0][1]), p, time_data)
     # print("The reference sequence: ", "ATTGTCTAGGCGACCA")
-    print("========================", "0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 ")
-    print("========================", "|  |  |  |  |  |  |  |  |  |  |  |  |  |  |  ")
+    print("========================", "0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15")
+    print("========================", "|  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |")
     print("The reference sequence: ", "A  T  T  G  T  C  T  A  G  G  C  G  A  C  C  A")
-    p = input("Please input short read: ")
+    p = input("Please input short read (type q or Q, then quit): ")
